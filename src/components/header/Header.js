b4182172fb15d0b6b6dbfabf9bf86c7aeafa1342
Tomio Koshikawa <tvk02jr@gmail.com>
2022-06-28 12:01:30 +0800

    added responsiveness

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,10 +8,10 @@ const Header = () => {
   return (
     <section id="header">
       <div className="d-flex flex-column ">
-        <div className="header__background d-flex flex-column justify-content-center">
+        <div className="header__background d-flex">
           <div className="mx-auto">
             <div>
-              <img src={logo} alt="Logo" />
+              <img src={logo} alt="Logo" className="img-fluid" />
               <span>The</span>
             </div>
             <h3>Fastest, Most Accurate</h3>
@@ -19,33 +19,39 @@ const Header = () => {
           </div>
         </div>
 
-        <div className="header__banner d-flex flex-row mx-auto">
+        <div className="header__banner d-flex flex-row mx-auto align-items-center py-1 px-3">
           <div className="banner__text col-4.5 mx-auto my-4">
             <h3 className="text-center"> SCHEDULE A DEMO</h3>
             <h4 className="text-center">Learn More about FloQast</h4>
           </div>
 
-          <div className="col-7.5">
-            <h4 className="banner__title mx-auto my-2">
+          <div className="col-7.5 container_">
+            <h4 className="banner__title text-center my-2">
               Learn How FloQast Can <span>Improve Your Month-End</span>
             </h4>
-            <Form className="d-flex flex-row justify-content-center align-items-column mx-4">
-              <Form.Group
-                className="mx-auto form__name"
-                controlId="form_firstName"
-              >
-                <Form.Control type="text" placeholder="First Name*" required />
+            <Form className="d-md-flex flex-row mx-md-5">
+              <Form.Group className="my-2" controlId="form_firstName">
+                <Form.Control
+                  className="form__name"
+                  type="text"
+                  placeholder="First Name*"
+                  required
+                />
               </Form.Group>
 
-              <Form.Group
-                className="mx-auto form__email"
-                controlId="form_email"
-              >
-                <Form.Control type="email" placeholder="Email*" required />
+              <Form.Group className="mx-md-3 my-2" controlId="form_email">
+                <Form.Control
+                  className="form__email"
+                  type="email"
+                  placeholder="Email*"
+                  required
+                />
               </Form.Group>
               <Button
+                className="my-2"
                 variant="flat"
                 type="submit"
+                size="sm"
                 style={{
                   backgroundColor: '#8bc540',
                   color: 'white',
@@ -56,8 +62,6 @@ const Header = () => {
               </Button>
             </Form>
           </div>
-
-          {/* ?? */}
         </div>
       </div>
     </section>
